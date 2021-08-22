aa101e554a0f1ae98311f83f32595523a5d86ad7
developer.niroj <developer.niroj@gmail.com>
2021-08-22 20:56:21 +0545

    Responseformatter class

---
+++ RestMiddleware/ResponseFormatter.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+
+namespace RestMiddleware
+{
+    public class ResponseFormatter
+    {
+        public static HttpResponseMessage BuildSuccessJson(object data)
+        {
+            var apiData = new { data };
+
+            var response = new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.OK };
+            response.Content = new StringContent(JsonConvert.SerializeObject(apiData), System.Text.Encoding.UTF8, "application/json");
+            return response;
+        }
+        public static HttpResponseMessage BuildSuccessJson()
+        {
+            var apiData = new
+            {
+
+            };
+            var response = new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.OK };
+            response.Content = new StringContent(JsonConvert.SerializeObject(apiData), System.Text.Encoding.UTF8, "application/json");
+            return response;
+        }
+
+        public static HttpResponseMessage BuildErrorsJson(string error, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+        {
+            var apiMessage = new
+            {
+                errors = new object[] {
+                    new {message = error}
+                }
+            };
+
+            var response = new HttpResponseMessage() { StatusCode = statusCode };
+            response.Content = new StringContent(JsonConvert.SerializeObject(apiMessage), System.Text.Encoding.UTF8, "application/json");
+            return response;
+        }
+
+        public static HttpResponseMessage BuildInfoJson(object data, string description, HttpStatusCode statusCode = HttpStatusCode.Continue)
+        {
+            var apiMessage = new
+            {
+                data,
+                description
+            };
+
+            var response = new HttpResponseMessage() { StatusCode = statusCode };
+            response.Content = new StringContent(JsonConvert.SerializeObject(apiMessage), System.Text.Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
